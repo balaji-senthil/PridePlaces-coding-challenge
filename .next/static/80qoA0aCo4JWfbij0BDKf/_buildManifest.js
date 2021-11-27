bfb1b2e9900f438df3e1b81eed37bb1d59a153c0
Balaji Senthilkumar <44097181+balaji-senthil@users.noreply.github.com>
2021-11-26 21:47:14 -0700

    test

---
+++ .next/static/80qoA0aCo4JWfbij0BDKf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/170-7d77f8134f9e65c337fa.js","static/chunks/pages/index-9c17fcd56eb0e8199ef9.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
